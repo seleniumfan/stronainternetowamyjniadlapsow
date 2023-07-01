a08f4a5c5d08bf26341d2001879be474589c56cf
Tomasz Iwanicki <tiko35@o2.pl>
2023-07-01 18:33:54 +0200

    Initial commit

---
+++ src/main/java/com/ti/stronainternetowamyjnadlapsow/StronaInternetowaMyjnaDlaPsowApplication.java
@@ -0,0 +1,13 @@
+package com.ti.stronainternetowamyjnadlapsow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StronaInternetowaMyjnaDlaPsowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StronaInternetowaMyjnaDlaPsowApplication.class, args);
+    }
+
+}
